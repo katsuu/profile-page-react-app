f4c0e4f198f7a18a7307ffb910ed4e787a8da836
Katrina Au <katrinaau95@gmail.com>
2020-04-21 00:10:24 +0100

    Added to data

--- src/data.js
+++ src/data.js
@@ -3,60 +3,64 @@ export default {
     name: {
       honorific: "Dr.",
       first: "Gregory",
-      last: "House"
+      last: "House",
     },
-    username: "gregoryr"
+    username: "gregoryr",
   },
   followRecommendations: [
     {
       id: 1,
       name: {
         first: "Sharlyne",
-        last: "Slassi"
+        last: "Slassi",
       },
       username: "slassi",
       bio: "This is a bio",
       avatar:
-        "https://avatars3.githubusercontent.com/u/22032643?s=460&u=ad7ed1c7d90b46ea9a45d187b11331b19722d277&v=4"
+        "https://avatars3.githubusercontent.com/u/22032643?s=460&u=ad7ed1c7d90b46ea9a45d187b11331b19722d277&v=4",
     },
     {
       id: 2,
       name: {
         first: "Katrina",
-        last: "Au"
+        last: "Au",
       },
       username: "katsuu",
       bio: "This is another bio",
       avatar:
-        "https://avatars0.githubusercontent.com/u/28890828?s=460&u=4fa6ce71389c99bd1be2f64dfe55240a107d269c&v=4"
+        "https://avatars0.githubusercontent.com/u/28890828?s=460&u=4fa6ce71389c99bd1be2f64dfe55240a107d269c&v=4",
     },
     {
       id: 3,
       name: {
         first: "Raphael",
-        last: "Essoo-Snowdon"
+        last: "Essoo-Snowdon",
       },
       username: "realph",
       bio: "This is ANOTHER bio",
-      avatar: "https://avatars3.githubusercontent.com/u/2334951?s=460&v=4"
+      avatar: "https://avatars3.githubusercontent.com/u/2334951?s=460&v=4",
     },
     {
       id: 4,
       name: {
         first: "ash",
-        last: "ketchum"
+        last: "ketchum",
       },
       username: "ashk",
       bio: "Gotta catch 'em all!",
-      avatar: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/7d13c5f2-28c5-468e-9d9b-f0048ce7921b/datudae-03bd1c1c-23f2-4d63-999e-ab55f4f4f484.png/v1/fill/w_471,h_471,q_80,strp/ash_ketchum__kalos_region__square_by_isaacnoeliscutie_datudae-fullview.jpg"
-    }
-  ]
+      avatar:
+        "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/7d13c5f2-28c5-468e-9d9b-f0048ce7921b/datudae-03bd1c1c-23f2-4d63-999e-ab55f4f4f484.png/v1/fill/w_471,h_471,q_80,strp/ash_ketchum__kalos_region__square_by_isaacnoeliscutie_datudae-fullview.jpg",
+    },
+    {
+      id: 5,
+      name: {
+        first: "kyle",
+        last: "wan",
+      },
+      username: "carlorn",
+      bio: "Love to code",
+      avatar:
+        "https://media-exp1.licdn.com/dms/image/C5603AQHij6gILH92_A/profile-displayphoto-shrink_800_800/0?e=1593043200&v=beta&t=04ZKm1rl3ED93IiHgclAW-FvltPN3Iv7k3LBUy6mais",
+    },
+  ],
 };
-
-// const kat = {
-//   name: {
-//     first: "Katrina",
-//     last: "Au"
-//   },
-//   username: "katsuu"
-// };
